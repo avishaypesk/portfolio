242230c7ba2a3e43e8eaf5b7fffb79fee70f6445
Avishay <avishaypesk@gmail.com>
2023-07-05 16:40:58 +0300

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../Header/Header'
+import Hero from '../Hero/Hero'
+import About from '../About/About'
+import Footer from '../Footer/Footer'
+
+function App() {
+  return (
+    <div className=''>App
+    <Header/>
+    <Hero/>
+    <About/>
+    <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        cyan: '#5FB4A2',
+        darkblue: '#203A4C',
+        grayishdarkblue: '#33323D',
+        verylightgrey: '#FAFAFA',
+        lightgrey: '#EAEAEB',
+        brightred: '#F43030',
+      },
+      fontFamily: {
+        'ibarra-real-nova': ['Ibarra Real Nova', 'sans-serif'],
+        'public-sans': ['Public Sans', 'sans-serif'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
