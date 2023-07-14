efddd8a58470007117ece1bada4442edcd93f92c
Avishay <avishaypesk@gmail.com>
2023-07-14 20:21:52 +0300

    add hero style on md

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link as ScrollLink } from 'react-scroll';
 import hero from '../../assets/images/hero.svg';
 import './Hero.css';
@@ -16,23 +16,56 @@ function Hero() {
     setIsHovered(false);
   };
 
+  const [isLargeScreen, setIsLargeScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 768);
+    };
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className='mt-12 relative'>
-      <img className="scale-x-[-1]" src={hero} alt='hero'/> 
-        <h1 className='font-ibarra-real-nova font-bold text-h2 text-grayishdarkblue tracking-tighter leading-42px'>Hey, I’m <span className='name'>Avishay Peskin</span> and I love building beautiful websites</h1>
-
-      <ScrollLink to='about' smooth={true} duration={600}>
-        <button
-          className='text-white uppercase font-public-sans bg-darkblue hover:bg-cyan flex items-center w-[200px] h-12 tracking-[2px] text-xs group mt-8'
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
-          <img src={isHovered ? buttonArrowWhite : buttonArrow} alt='button arrows' className='bg-darkerblue group-hover:bg-cyan' />
-          <span className="flex-grow text-center">About me</span>
-        </button>
-      </ScrollLink>
+      <div className='relative'>
+        <img className='scale-x-[-1] md:h-full' src={hero} alt='hero' />
+        {!isLargeScreen && (
+          <div className='bg-white'>
+            <h1 className='font-ibarra-real-nova font-bold text-h2 text-grayishdarkblue tracking-tighter leading-42px pt-6 md:pt-14 md:pr-14'>
+              Hey, I’m <span className='name'>Avishay Peskin</span> and I love building beautiful websites
+            </h1>
+          </div>
+        )}
+        <div className={isLargeScreen ? 'absolute bottom-0 left-0 w-3/4 bg-white' : 'mt-4'}>
+          {isLargeScreen && (
+            <h1 className='font-ibarra-real-nova font-bold text-h2 text-grayishdarkblue tracking-tighter leading-42px md:pt-14 md:pr-14'>
+              Hey, I’m <span className='name'>Avishay Peskin</span> and I love building beautiful websites
+            </h1>
+          )}
+          <ScrollLink to='about' smooth={true} duration={600}>
+            <button
+              className='text-white uppercase font-public-sans bg-darkblue hover:bg-cyan flex items-center w-[200px] h-12 tracking-[2px] text-xs group mt-8 md:mt-12'
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              <img
+                src={isHovered ? buttonArrowWhite : buttonArrow}
+                alt='button arrows'
+                className='bg-darkerblue group-hover:bg-cyan'
+              />
+              <span className='flex-grow text-center'>About me</span>
+            </button>
+          </ScrollLink>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Hero;
