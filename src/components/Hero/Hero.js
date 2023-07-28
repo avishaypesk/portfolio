55c6d9375968f019b47e2d5cc755064bf8ef5eaf
Avishay <avishaypesk@gmail.com>
2023-07-29 00:41:32 +0300

    edit hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -34,7 +34,7 @@ function Hero() {
   return (
     <div className='mt-12 relative'>
       <div className='relative'>
-        <img className='scale-x-[-1] md:h-full xl:w-full xl:max-h-[46rem] m-auto rounded-2xl xl:bg-white' src={hero} alt='hero' />
+        <img className='scale-x-[-1] md:h-full xl:w-full xl:max-h-[46rem] xl:m-auto rounded-2xl xl:bg-white' src={hero} alt='hero' />
         {!isLargeScreen && (
           <div>
             <h1 className='font-ibarra-real-nova font-bold text-h2 text-grayishdarkblue tracking-tighter leading-42px pt-6 md:pt-14 md:pr-14'>
@@ -42,7 +42,7 @@ function Hero() {
             </h1>
           </div>
         )}
-        <div className={isLargeScreen ? 'absolute bottom-0 left-0 w-3/5 xl:w-[650px] bg-white xl:rounded-t-2xl' : 'mt-4'}>
+        <div className={isLargeScreen ? 'absolute bottom-0 left-0 xl:left-20 w-3/5 xl:w-[650px] bg-white xl:rounded-t-2xl' : 'mt-4'}>
           {isLargeScreen && (
             <h1 className='font-ibarra-real-nova font-bold text-h2 xl:text-[64px] text-grayishdarkblue tracking-tighter leading-42px xl:leading-[65px] md:pt-14 md:pr-14'>
               Hey, I’m <span className='name'>Avishay Peskin</span> and I love building beautiful websites
